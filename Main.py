c72e899d4c18626221ba008b2c76bb554c7e21dd
Jerónimo Martínez <110127604+Lechuza22@users.noreply.github.com>
2025-01-21 13:17:56 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ from sklearn.model_selection import train_test_split
 def load_data():
     locacion_y_coordenadas = pd.read_csv('LocacionYcoordenadas.csv')
     yellow_trip_data = pd.read_csv('viajes_taxis_Amarilo_reducido.csv')
-    vehicle_fuel_economy = pd.read_csv('transformed_Vehicle Fuel Economy Data.csv')
+    vehicle_fuel_economy = pd.read_csv('Vehicle_Fuel_Data_Updated.csv')
     return locacion_y_coordenadas, yellow_trip_data, vehicle_fuel_economy
 
 locacion_y_coordenadas, yellow_trip_data, vehicle_fuel_economy = load_data()
